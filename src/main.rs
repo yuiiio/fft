4726cacde3b999a994cca8e7c832f00c415fa296
yuiiio <atbjyk@protonmail.com>
2023-10-21 17:49:30 +0900

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-const PI_2_32: f32 = 2.0 as f32 * std::f32::consts::PI;
-
-const SAMPLE_SIZE_POWER_OF_TWO: u32 = 6;// 64 = 2^6
-const SAMPLE_SIZE: u32 = 2u32.pow(SAMPLE_SIZE_POWER_OF_TWO);
-
-const ADC_BIT: u32 = 12; // 10 or 12 bit adc
-const ADC_BIT_MAX: u32 = 2u32.pow(ADC_BIT);
-
+// const PI_2_32: f32 = 2.0 as f32 * std::f32::consts::PI;
 const REF_DATA_VALUE_MAX: u32 = 64;// (1/64) is minimum value
-const BALL_PULSE: [u32; SAMPLE_SIZE as usize] =
+
+// reference from https://github.com/vha3/Hunter-Adams-RP2040-Demos/blob/master/Audio/g_Audio_FFT/fft.c
+const NUM_SAMPLES: usize = 64;// (1/64) is minimum value
+const LOG2_NUM_SAMPLES: u16 = 6;// 64 = 2^6
+// Length of short (16 bits) minus log2 number of samples (6)
+const SHIFT_AMOUNT: u16 = 10;//
+
+const BALL_PULSE: [u16; NUM_SAMPLES] =
 [
     0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, // first FULL: 8 pulse
     0, 16, 0, 16, 0, 16, 0, 16, // second 1/4: 4 pulse
@@ -15,168 +15,97 @@ const BALL_PULSE: [u32; SAMPLE_SIZE as usize] =
     0, 1, 0, 1, 0, 1, 0, 1, // second 1/64: 4 pulse
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 64 - ((8+4+4+4)*2) = 24
-]; // need revert for sensor input value ?
-   // and need 10bit (1024) or 12bit (4096) scale is need ?
-   // or only wave signal is important ?
+];
 
-//should calc only onece on.
-fn bit_reverse_order() -> [u32; SAMPLE_SIZE as usize] {
-    let mut default_order: [u32; SAMPLE_SIZE as usize] = [0; SAMPLE_SIZE as usize];
-    for i in 0..SAMPLE_SIZE {
-        default_order[i as usize] = i
-    }
-
-    let mut reverse_order: [u32; SAMPLE_SIZE as usize] = [0; SAMPLE_SIZE as usize];
-
-    let bits_size: usize = SAMPLE_SIZE_POWER_OF_TWO as usize;
-
-    // bit num use u32
-    let shift_bit: usize = 32 - bits_size;
-
-    for i in 0..SAMPLE_SIZE {
-        reverse_order[i as usize] = default_order[i as usize].reverse_bits() >> shift_bit;
-    }
-
-    return reverse_order;
+fn multfix15(a: i16, b: i16) -> i16 {
+    ((a as i32 * b as i32) >> 15) as i16
 }
 
-#[derive(Clone)]
-#[derive(Copy)]
-#[derive(Debug)]
-pub struct complex_t {
-    x: f32,
-    i: f32,
-}
+static mut Sinewave: [i16; NUM_SAMPLES] = [0; NUM_SAMPLES];
+static mut window: [i16; NUM_SAMPLES] = [0; NUM_SAMPLES];
 
-impl complex_t {
-    pub fn new(x: f32, i: f32) -> Self {
-        complex_t { x, i }
-    }
-
-    pub fn add(&self, a: complex_t) -> Self {
-        let bx: f32 = self.x + a.x;
-        let bi: f32 = self.i + a.i;
-        complex_t {x: bx, i: bi}
-    }
-
-    pub fn sub(&self, a: complex_t) -> Self {
-        let bx: f32 = self.x - a.x;
-        let bi: f32 = self.i - a.i;
-        complex_t {x: bx, i: bi}
-    }
-
-    pub fn mul(&self, a: complex_t) -> Self {
-        let bx: f32 = (self.x * a.x) - (self.i * a.i);
-        let bi: f32 = (self.x * a.i) + (self.i * a.x);
-        complex_t {x: bx, i: bi}
-    }
-}
-
-// should calc only onece.
-// should make rotate table at startup.
-fn rotate_w_matrix() -> [[complex_t; SAMPLE_SIZE as usize]; SAMPLE_SIZE_POWER_OF_TWO as usize] {
-    let mut rotate_w_matrix:[[complex_t; SAMPLE_SIZE as usize]; SAMPLE_SIZE_POWER_OF_TWO as usize] =
-        [[complex_t {x: 0.0, i: 0.0}.clone(); SAMPLE_SIZE as usize]; SAMPLE_SIZE_POWER_OF_TWO as usize];
-
-    for i in (0..SAMPLE_SIZE_POWER_OF_TWO).rev() {
-        let block_num: u32 = 2u32.pow(i);
-        let block_size: u32 = SAMPLE_SIZE / block_num;
-
-        let n: f32 = (2.0 as f32).powi((SAMPLE_SIZE_POWER_OF_TWO - i) as i32);
-        let dir: f32 =  PI_2_32 / n;
-        let rotate_w_first: complex_t = complex_t { x: dir.cos(), i: dir.sin() };
-
-        let mut rotate_w_vec: Vec<complex_t> = vec!();
-        let mut rotate_w: complex_t = complex_t {x: 1.0, i: 0.0};
-        for _j in 0..block_size {
-            rotate_w_vec.push(rotate_w);
-            rotate_w = rotate_w.mul(rotate_w_first);
-        }
-
-        for j in 0..SAMPLE_SIZE {
-            let wk: u32 = j % block_size;
-            rotate_w_matrix[((SAMPLE_SIZE_POWER_OF_TWO - 1) - i) as usize][j as usize] = rotate_w_vec[wk as usize];
-        }
-    }
-    return rotate_w_matrix;
-}
-
-// input u32 vector and output f32 vector.
-fn fft(input_vec: &[u32; SAMPLE_SIZE as usize],
-       reverse_order: &[u32; SAMPLE_SIZE as usize],
-       rotate_w_matrix: &[[complex_t; SAMPLE_SIZE as usize]; SAMPLE_SIZE_POWER_OF_TWO as usize]) -> [f32; SAMPLE_SIZE as usize] {
-
-    let mut input_reverse_order: [complex_t; SAMPLE_SIZE as usize] = [complex_t{ x: 0.0, i: 0.0 }; SAMPLE_SIZE as usize];
-
-    //and expect x-axis symmetric so, sub avg num.
-    let mut avg_num: f32 = 0.0;
-    for i in 0..SAMPLE_SIZE {
-        avg_num += input_vec[i as usize] as f32;
-    }
-    avg_num = avg_num / (SAMPLE_SIZE as f32);
-
+fn FFTfix(fr: &mut [i16; NUM_SAMPLES], fi: &mut [i16; NUM_SAMPLES],) -> () {
     //bit order reverse
-    for i in 0..SAMPLE_SIZE {
-        input_reverse_order[i as usize].x = input_vec[reverse_order[i as usize] as usize] as f32 - avg_num;
+    for m in 1..(NUM_SAMPLES - 1) {
+        // swap odd and even bits
+        let mr = ((m >> 1) & 0x5555) | ((m & 0x5555) << 1);
+        // swap consecutive pairs
+        mr = ((mr >> 2) & 0x3333) | ((mr & 0x3333) << 2);
+        // swap nibbles ... 
+        mr = ((mr >> 4) & 0x0F0F) | ((mr & 0x0F0F) << 4);
+        // swap bytes
+        mr = ((mr >> 8) & 0x00FF) | ((mr & 0x00FF) << 8);
+        // shift down mr
+        mr >>= SHIFT_AMOUNT ;
+        // don't swap that which has already been swapped
+        if mr<=m { continue; }
+        // swap the bit-reveresed indices
+        let tr = fr[m] ;
+        fr[m] = fr[mr] ;
+        fr[mr] = tr ;
+        let ti = fi[m] ;
+        fi[m] = fi[mr] ;
+        fi[mr] = ti ;
     }
+    // Adapted from code by:
+    // Tom Roberts 11/8/89 and Malcolm Slaney 12/15/94 malcolm@interval.com
+    // Length of the FFT's being combined (starts at 1)
+    let mut L: usize = 1 ;
+    // Log2 of number of samples, minus 1
+    let mut k: u16 = LOG2_NUM_SAMPLES - 1 ;
+    // While the length of the FFT's being combined is less than the number 
+    // of gathered samples . . .
+    while L < NUM_SAMPLES {
+        // Determine the length of the FFT which will result from combining two FFT's
+        let istep: usize = L<<1 ;
+        // For each element in the FFT's that are being combined . . .
+        for m in 0..L {
+            let j = m << k;
+            let wr =  Sinewave[j + NUM_SAMPLES/4] ; // cos(2pi m/N)
+            let wi = -Sinewave[j] ;                 // sin(2pi m/N)
+            wr >>= 1 ;                          // divide by two
+            wi >>= 1 ;                          // divide by two
+            // i gets the index of one of the FFT elements being combined
+            let mut i: usize = m;
+            while i < NUM_SAMPLES {
+                // j gets the index of the FFT element being combined with i
+                let j = i + L ;
+                // compute the trig terms (bottom half of the above matrix)
+                let tr = multfix15(wr, fr[j]) - multfix15(wi, fi[j]) ;
+                let ti = multfix15(wr, fi[j]) + multfix15(wi, fr[j]) ;
+                // divide ith index elements by two (top half of above matrix)
+                let qr = fr[i]>>1 ;
+                let qi = fi[i]>>1 ;
+                // compute the new values at each index
+                fr[j] = qr - tr ;
+                fi[j] = qi - ti ;
+                fr[i] = qr + tr ;
+                fi[i] = qi + ti ;
 
-    let mut output_vec: [complex_t; SAMPLE_SIZE as usize] = input_reverse_order;
-    for i in (0..SAMPLE_SIZE_POWER_OF_TWO).rev() {
-        let middle_vec: [complex_t; SAMPLE_SIZE as usize] = output_vec;
-
-        let block_num: u32 = 2u32.pow(i);
-        let block_size: u32 = SAMPLE_SIZE / block_num;
-        let block_size_div2: u32 = block_size / 2;
-
-        for j in 0..block_num {
-            let block_start_point: u32 = j * block_size; // block_size is 2 ~ SAMPLE_SIZE
-            for k in 0..block_size_div2 {
-                let point1: u32 = block_start_point + k;
-                let point2: u32 = block_start_point + (k * 2);
-
-                let output_point: u32 = point1;
-                output_vec[output_point as usize] = middle_vec[point1 as usize].add( rotate_w_matrix[i as usize][output_point as usize].mul(middle_vec[point2 as usize]) );
-            }
-            for k in 0..block_size_div2 {
-                let point1: u32 = block_start_point + k;
-                let point2: u32 = block_start_point + (k * 2);
-
-                let output_point: u32 = point1 + block_size_div2;
-                output_vec[output_point as usize] = middle_vec[point1 as usize].add( rotate_w_matrix[i as usize][output_point as usize].mul(middle_vec[point2 as usize]) );
+                i += istep;
             }
         }
+        k = k - 1;
+        L = istep;
     }
-
-    let mut output_x_vec: [f32; SAMPLE_SIZE as usize] = [0.0; SAMPLE_SIZE as usize];
-    for i in 0..SAMPLE_SIZE {
-        output_x_vec[i as usize] = output_vec[i as usize].x;
-    }
-    return output_x_vec;
 }
 
 fn main() {
-    let mut sensor_val: [u32; SAMPLE_SIZE as usize] = [0; SAMPLE_SIZE as usize]; //10 or 12 bit adc < 32 bit
-
-    // set sample data scale and fit.
-    for i in 0..SAMPLE_SIZE {
-        sensor_val[i as usize] = ADC_BIT_MAX - ((ADC_BIT_MAX / REF_DATA_VALUE_MAX) * BALL_PULSE[i as usize]);
-    }
-
     //sin wave for test
     /*
-    for i in 0..SAMPLE_SIZE {
-        let x: f32 = PI_2_32 * (i as f32 / SAMPLE_SIZE as f32);
+    for i in 0..NUM_SAMPLES {
+        let x: f32 = PI_2_32 * (i as f32 / NUM_SAMPLES as f32);
         sensor_val[i as usize] = ((ADC_BIT_MAX as f32 * 1.0/2.0) + ((ADC_BIT_MAX as f32 * x.sin()) * 1.0/2.0)) as u32;
     }
     */
 
     //display
-    const NX: u32 = SAMPLE_SIZE;
+    const NX: usize = NUM_SAMPLES;
     const NY: u32 = REF_DATA_VALUE_MAX;//40;//(TERMINAL $LINES)
 
     let mut plot: [[bool; NY as usize]; NX as usize] =[[false; NY as usize]; NX as usize];
 
-    for x in 0..SAMPLE_SIZE {
+    for x in 0..NUM_SAMPLES {
         let y: usize = (sensor_val[x as usize] as f32 * ((NY - 1) as f32 / ADC_BIT_MAX as f32) as f32) as usize;
         plot[x as usize][y as usize] = true;
     }
@@ -193,21 +122,15 @@ fn main() {
         println!("");
     }
 
-    // reverse bit order and w rotate matrix should calc only onece.
-    let reverse_order: [u32; SAMPLE_SIZE as usize] = bit_reverse_order();
-    //println!("{:?}", reverse_order);
-    let rotate_w_matrix: [[complex_t; SAMPLE_SIZE as usize]; SAMPLE_SIZE_POWER_OF_TWO as usize] = rotate_w_matrix();
-    //println!("{:?}", rotate_w_matrix);
-
     //try fft
-    let fft_result: [f32; SAMPLE_SIZE as usize] = fft(&sensor_val, &reverse_order, &rotate_w_matrix);
+    let fft_result: [f32; NUM_SAMPLES as usize] = fft(&sensor_val, &reverse_order, &rotate_w_matrix);
     println!("{:?}", fft_result);
 
     //display
 
     let mut plot: [[bool; NY as usize]; NX as usize] =[[false; NY as usize]; NX as usize];
-    for x in 0..SAMPLE_SIZE {
-        let y: f32 = NY as f32 - ((fft_result[x as usize].abs() * NY as f32 / (ADC_BIT_MAX * SAMPLE_SIZE) as f32) as f32)  - 1.0;
+    for x in 0..NUM_SAMPLES {
+        let y: f32 = NY as f32 - ((fft_result[x as usize].abs() * NY as f32 / (ADC_BIT_MAX * NUM_SAMPLES) as f32) as f32)  - 1.0;
         println!("{}", y);
         let y: usize = y as usize;
         plot[x as usize][y as usize] = true;
