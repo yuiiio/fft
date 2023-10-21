396d1e06f095e075a767423b9bad2d98a850d91f
yuiiio <atbjyk@protonmail.com>
2023-10-21 19:58:57 +0900

    WIP2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 const REF_DATA_VALUE_MAX: u32 = 64;// (1/64) is minimum value
 
 // reference from https://github.com/vha3/Hunter-Adams-RP2040-Demos/blob/master/Audio/g_Audio_FFT/fft.c
-const NUM_SAMPLES: usize = 64;// (1/64) is minimum value
-const LOG2_NUM_SAMPLES: u16 = 6;// 64 = 2^6
+const NUM_SAMPLES: usize = 128;// (1/64) is minimum value
+const LOG2_NUM_SAMPLES: u16 = 7;// 64 = 2^6
 // Length of short (16 bits) minus log2 number of samples (6)
-const SHIFT_AMOUNT: u16 = 10;//
+const SHIFT_AMOUNT: u16 = 9;//
 
 const BALL_PULSE: [u16; NUM_SAMPLES] =
 [
+    0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, // first FULL: 8 pulse
+    0, 16, 0, 16, 0, 16, 0, 16, // second 1/4: 4 pulse
+    0, 4, 0, 4, 0, 4, 0, 4, // second 1/16: 4 pulse
+    0, 1, 0, 1, 0, 1, 0, 1, // second 1/64: 4 pulse
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 64 - ((8+4+4+4)*2) = 24
     0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, 0, 64, // first FULL: 8 pulse
     0, 16, 0, 16, 0, 16, 0, 16, // second 1/4: 4 pulse
     0, 4, 0, 4, 0, 4, 0, 4, // second 1/16: 4 pulse
